d25beb71cca2fb249a22bc46e2beccf7b7d2d964
codenametoba <tobadesola@gmail.com>
2024-12-16 16:40:15 +0100

    Add LightStream as part of the main repository

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ import { connectDB } from './config/db.js';
 import upload from './middlewares/uploadMiddleware.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 // import app from './src/app.js';
+import cors from 'cors'
+
 
 const app = express();
 const PORT = ENV_VARS.PORT
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
 app.use(express.json()); // will allow us to parse req.body
 app.use("/api/v1/auth" ,authRoutes);
 app.use("/api/v1/upload", uploadRoutes);
